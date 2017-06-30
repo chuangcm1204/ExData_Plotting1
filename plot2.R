9dc35937cd33d1fd799852d27823ec9e7945c39f
chuangcm1204 <chuangcm@tsmc.com>
2017-06-30 10:34:30 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+Sys.setlocale(category = "LC_ALL", locale = "english")
 dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
